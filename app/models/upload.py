06d0dd2544f85dc04adfc893a557edff6de0e928
Marcus McHale <marcus.mchale@nuigalway.ie>
2020-02-25 15:19:33 +0000

    fix for item_source_error_check

--- app/models/upload.py
+++ app/models/upload.py
@@ -1134,8 +1134,8 @@ class PropertyUpdateHandler:
 					)
 				errors[-1] += ', '.join([
 					'(source uid: ' + str(i['ancestor']) + ', source variety:' + str(i['ancestor_variety']) + ', ' +
-					'dependent item: ' + str(i['descendant']) + ', ' + 'dependant variety: ' + str(i['dependant_variety']) +
-					')'
+					'descendant item: ' + str(i['descendant']) + ', ' + 'descendant variety: ' + str(i['descendant_variety']) +
+					')'item source error cehck
 					for i in record[0]['variety_conflicts'][0:2]
 				])
 			elif record[0]['tree_varieties_error']:
