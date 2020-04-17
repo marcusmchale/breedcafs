cce3c106cdb9e360fa3f946f5d407a42140e6828
Marcus McHale <marcus.mchale@nuigalway.ie>
2020-04-17 12:09:22 +0100

    re-enable email

--- app/emails.py
+++ app/emails.py
@@ -11,7 +11,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 	msg = Message(subject, sender=sender, recipients=recipients)
 	msg.body = text_body
 	msg.html = html_body
-	# mail.send(msg)
+	mail.send(msg)
 
 
 def send_attachment(subject, sender, recipients, text_body, html_body, filename, mimetype, file):
@@ -19,7 +19,7 @@ def send_attachment(subject, sender, recipients, text_body, html_body, filename,
 	msg.body = text_body
 	msg.html = html_body
 	msg.attach(filename, mimetype, file.getvalue())
-	# mail.send(msg)
+	mail.send(msg)
 
 
 def send_static_attachment(subject, sender, recipients, text_body, html_body, filename, mimetype, file_path):
@@ -28,4 +28,4 @@ def send_static_attachment(subject, sender, recipients, text_body, html_body, fi
 	msg.html = html_body
 	with app.open_instance_resource(file_path, 'r') as file:
 		msg.attach(str(filename), mimetype, file.read())
-	# mail.send(msg)
+	mail.send(msg)
